8c1b4774f2401f29496f7f13cc8a3afb15f30b33
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2025-03-24 19:25:25 +0200

    Initial Commit

---
+++ SwiftUIComponents/SwiftUIComponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIComponentsApp.swift
+//  SwiftUIComponents
+//
+//  Created by Manyuchi, Carrington C on 2025/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIComponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
